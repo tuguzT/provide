01430af0a94c2f16a20216d5a555ede6d3ec8ed7
tuguzT <timurka.tugushev@gmail.com>
2023-09-22 16:30:52 +0300

    Test `ProvideWith` trait with empty context

--- tests/context.rs
+++ tests/context.rs
@@ -1,24 +1,35 @@
-use provide::{with::ProvideRefWith, ProvideRef};
+use provide::{
+    with::{ProvideRefWith, ProvideWith},
+    ProvideRef,
+};
+
+#[test]
+fn empty() {
+    let provider = 1;
+    let (dependency, _): (i32, _) = provider.provide_with(());
+    assert_eq!(dependency, 1);
+}
 
 #[test]
 fn my_context_ref() {
-    struct ProviderWrapper<T>(T)
+    struct GenericProvider<T>(T)
     where
         T: ?Sized;
 
     struct WrapOption;
 
-    impl<'me, T, U> ProvideRefWith<'me, Option<T>, WrapOption> for ProviderWrapper<U>
+    impl<'me, T, U> ProvideRefWith<'me, Option<T>, WrapOption> for GenericProvider<U>
     where
-        U: ProvideRef<'me, T>,
+        U: ProvideRef<'me, T> + ?Sized,
     {
         fn provide_ref_with(&'me self, _: WrapOption) -> Option<T> {
             let Self(provider) = self;
-            Some(provider.provide_ref())
+            let dependency = provider.provide_ref();
+            Some(dependency)
         }
     }
 
-    let provider = ProviderWrapper("hello");
+    let provider = GenericProvider("hello");
     let dependency = provider.provide_ref_with(WrapOption);
     assert_eq!(dependency, Some("hello"));
 }
