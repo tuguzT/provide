73179789bdfc78a9cdf438ddb6a25a5e1aa39f0d
tuguzT <timurka.tugushev@gmail.com>
2024-06-21 20:13:07 +0300

    Remove injectors from documentation (the same as providers)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 //! Truly zero cost dependency injection — in safe and stable Rust.
 //!
-//! This crate defines some key concepts:
+//! This crate defines two key concepts:
 //! - **providers** are types which provide some dependency by value, shared or unique reference
 //! - **context** types represent different ways to provide some dependency
-//! - **injectors** are types which create requested values from contained dependencies
 //!
 //! // TODO better documentation
 
