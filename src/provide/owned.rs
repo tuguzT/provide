3c46ddc9144193aa698a78f34d4f86e867d432a0
tuguzT <timurka.tugushev@gmail.com>
2024-09-01 15:09:53 +0300

    Move integration test of `Provide` into documentation tests

--- src/provide/owned.rs
+++ src/provide/owned.rs
@@ -16,10 +16,101 @@ pub trait Provide<T>: Sized {
     ///
     /// # Examples
     ///
+    /// You can implement this trait for your provider
+    /// to provide dependency by value:
+    ///
     /// ```
     /// use provide::Provide;
     ///
-    /// todo!()
+    /// struct Provider {
+    ///     foo: i32,
+    ///     bar: f32,
+    /// }
+    ///
+    /// impl Provide<i32> for Provider {
+    ///     type Remainder = f32;
+    ///
+    ///     fn provide(self) -> (i32, Self::Remainder) {
+    ///         let Self { foo, bar } = self;
+    ///         (foo, bar)
+    ///     }
+    /// }
+    ///
+    /// let provider = Provider { foo: 1, bar: 2.0 };
+    /// let (dependency, _): (i32, _) = provider.provide();
+    /// assert_eq!(dependency, 1);
+    /// ```
+    ///
+    /// You can also provide your provider itself as dependency
+    /// thanks to implementation for all types which implement [`Into`]:
+    ///
+    /// ```
+    /// use provide::Provide;
+    ///
+    /// struct Provider(i32);
+    ///
+    /// impl From<Provider> for i32 {
+    ///     fn from(provider: Provider) -> Self {
+    ///         let Provider(dependency) = provider;
+    ///         dependency
+    ///     }
+    /// }
+    ///
+    /// let provider = Provider(1);
+    /// let (dependency, _): (i32, _) = provider.provide();
+    /// assert_eq!(dependency, 1);
+    /// ```
+    ///
+    /// However, due to this blanket implementation, you cannot do this:
+    ///
+    /// ```compile_fail
+    /// use provide::Provide;
+    ///
+    /// // Provides generic values.
+    /// struct GenericProvider<T>(T);
+    ///
+    /// impl<T> Provide<T> for GenericProvider<T> {
+    ///     type Remainder = ();
+    ///
+    ///     fn provide(self) -> (T, Self::Remainder) {
+    ///         let Self(dependency) = self;
+    ///         (dependency, ())
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// Instead, consider using a newtype wrapper to avoid conflicting implementations:
+    ///
+    /// ```
+    /// use provide::Provide;
+    ///
+    /// // Provides generic values.
+    /// struct GenericProvider<T>(T);
+    ///
+    /// // Newtype for dependency provided by `GenericProvider`.
+    /// struct Wrapper<T>(T);
+    ///
+    /// impl<T> Provide<Wrapper<T>> for GenericProvider<T> {
+    ///     type Remainder = ();
+    ///
+    ///     fn provide(self) -> (Wrapper<T>, Self::Remainder) {
+    ///         let Self(dependency) = self;
+    ///         (Wrapper(dependency), ())
+    ///     }
+    /// }
+    ///
+    /// // Type of our dependency.
+    /// #[derive(Debug, PartialEq)]
+    /// struct MyDependency {
+    ///     foo: i32,
+    ///     bar: f32,
+    /// }
+    ///
+    /// let dependency = MyDependency { foo: 1, bar: 2.0 };
+    /// let provider = GenericProvider(dependency);
+    ///
+    /// let (Wrapper(dependency), _): (Wrapper<_>, _) = provider.provide();
+    /// assert_eq!(dependency, MyDependency { foo: 1, bar: 2.0 });
     /// ```
     fn provide(self) -> (T, Self::Remainder);
 }
